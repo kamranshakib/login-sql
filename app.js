96a39e0d6c2aa13bc71278865b29ec9c6f510e4e
Kamran <Kamranshakib371@gmail.com>
2025-01-20 23:10:40 -0800

    connect to database and finish

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const mysql = require('mysql');
 const app = express()
 app.use(express.urlencoded({extended : true}))
 const port = 3000
+var cont = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'adminform'
+})
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var id = 0;
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html')
+});
+
+app.post('/',(req,res)=>{
+    let {EmailAdmin, passwordAdmin} = req.body;
+    id = id + 1;
+    cont.query(`insert into admintable2 values (${id}, '${EmailAdmin}', '${passwordAdmin}')`)
+    res.redirect('/')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
